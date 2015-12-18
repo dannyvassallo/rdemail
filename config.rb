1291355f5c2041466cf52cb896623f17e7106335
dannyvassallo <danielvassallo87@gmail.com>
2015-12-18 14:55:56 -0500

    nokogiri is being stupid. disney layout added

--- config.rb
+++ config.rb
@@ -1,11 +1,10 @@
-require "middleman-inline-style"
+# require "middleman-inline-style"
+# activate :inline_style
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
+
 configure :build do
-  activate :inline_style
 end
